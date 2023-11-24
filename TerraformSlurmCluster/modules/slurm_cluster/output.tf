c701dc3b2a7b152d14a9ad5bab3a101848b68c84
Mohamad L'Emir Chehab <mohamad.lemir-chehab@uni-tuebingen.de>
2023-11-24 16:05:48 +0100

    Update output names

--- TerraformSlurmCluster/modules/slurm_cluster/output.tf
+++ TerraformSlurmCluster/modules/slurm_cluster/output.tf
@@ -1,18 +1,18 @@
-output "compute_ip" {
+output "slurm-compute" {
   value = [
     for compute in openstack_compute_instance_v2.terraform-slurm-compute:
     "${compute.name}, ${element(compute.network.*.fixed_ip_v4, 1)}, ${element(compute.network.*.fixed_ip_v6, 0)}"
   ]
 }
 
-output "controller_ip" {
+output "slurm-controller" {
   value = join(", ", [openstack_compute_instance_v2.terraform-slurm-controller.name, openstack_compute_instance_v2.terraform-slurm-controller.network.1.fixed_ip_v4, openstack_compute_instance_v2.prometheus-server.network.0.fixed_ip_v6])
 }
 
-output "prometheus_ip" {
+output "prometheus" {
   value = join(", ", [openstack_compute_instance_v2.prometheus-server.name, openstack_compute_instance_v2.prometheus-server.network.1.fixed_ip_v4, openstack_compute_instance_v2.prometheus-server.network.0.fixed_ip_v6])
 }
 
-output "grafana_ip" {
+output "grafana" {
   value = join(", ", [openstack_compute_instance_v2.grafana.name, openstack_compute_instance_v2.grafana.access_ip_v6])
 }
