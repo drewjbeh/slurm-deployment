93cfab697978bbcf972c44fe6bda562378898c10
Ubuntu <drewjbeh@gmail.com>
2024-01-08 15:14:25 +0000

    Add a remote-exec block to mount the volume on the controller VM once it's created

--- TerraformSlurmCluster/modules/slurm_cluster/volume.tf
+++ TerraformSlurmCluster/modules/slurm_cluster/volume.tf
@@ -8,5 +8,19 @@ resource "openstack_blockstorage_volume_v3" "nfs-ceph" {
 resource "openstack_compute_volume_attach_v2" "nfs-ceph-attach" {
   instance_id = openstack_compute_instance_v2.terraform-slurm-controller.id
   volume_id   = openstack_blockstorage_volume_v3.nfs-ceph.id
-  depends_on = [openstack_compute_instance_v2.terraform-slurm-controller]
+  depends_on  = [openstack_compute_instance_v2.terraform-slurm-controller]
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = "${openstack_compute_instance_v2.terraform-slurm-controller.network.0.fixed_ip_v6}"
+      private_key = "${file(var.key_path)}"
+    }
+    inline = [
+      "sudo mkdir -p /mnt/ceph",
+      "yes | sudo mkfs -t ext4 /dev/vdb",
+      "echo '/dev/vdb   /mnt/ceph   ext4   defaults   0 2' | sudo tee -a /etc/fstab",
+      "sudo mount -a"
+    ]
+  }
 }
